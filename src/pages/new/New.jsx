998128834c30a8c0790cc00e3b42f6caf49cd566
Vaodi <95374096+Vaodi@users.noreply.github.com>
2022-06-03 03:20:08 -0400

    Initial version dashboard

---
+++ src/pages/new/New.jsx
@@ -0,0 +1,13 @@
+import "./new.scss"
+import React, { Component }  from 'react';
+
+
+const New = () => {
+    return (
+        <div>
+            New
+        </div>
+    )
+}
+
+export default New
